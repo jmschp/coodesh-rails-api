8ee5f9d05721738f405d4bd6ba9bb7468425174e
Miguel Pimenta <jmiguelpimenta@gmail.com>
2021-01-13 14:31:53 -0300

    changed column name type to category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
-PRODUCT_TYPE = ['dairy', 'fruit', 'vegetable', 'bakery', 'vegan', 'meat']
-PRODUCT_RATING = [1, 2, 3, 4, 5]
+PRODUCT_CATEGORY = ['dairy', 'fruit', 'vegetable', 'bakery', 'vegan', 'meat']
+PRODUCT_RATING = [0, 1, 2, 3, 4, 5]
 class Product < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
-  validates :type, presence: true, inclusion: { in: PRODUCT_TYPE }
+  validates :category, presence: true, inclusion: { in: PRODUCT_CATEGORY }
   validates :description, length: { maximum: 500 }
   validates :price, presence: true
   validates :rating, presence: true, inclusion: { in: PRODUCT_RATING }
--- db/migrate/20210113163734_create_products.rb
+++ db/migrate/20210113163734_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
       t.string :title, null: false
-      t.string :type, null: false
+      t.string :category, null: false
       t.text :description
       t.decimal :price, null: false, default: 0
       t.integer :rating, null: false, default: 0
