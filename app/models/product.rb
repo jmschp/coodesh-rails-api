506d4ed15c8f516d4c80914c7436f46a4a188ece
Miguel Pimenta <jmiguelpimenta@gmail.com>
2021-01-13 13:56:20 -0300

    Added produc validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
+PRODUCT_TYPE = []
+PRODUCT_RATING = [1, 2, 3, 4, 5]
 class Product < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :type, presence: true, inclusion: { in: PRODUCT_TYPE }
+  validates :description, length: { maximum: 500 }
+  validates :price, presence: true
+  validates :rating, presence: true, inclusion: { in: PRODUCT_RATING }
 end
