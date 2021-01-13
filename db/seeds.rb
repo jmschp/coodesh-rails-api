3748c8b3c32de02254d9bc5b326f6a98f3b72f7d
Miguel Pimenta <jmiguelpimenta@gmail.com>
2021-01-13 14:32:08 -0300

    created seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+
+filepath = 'products.json'
+
+serialized_products = File.read(filepath)
+
+products = JSON.parse(serialized_products)
+
+products.each do |product|
+  Product.create!(
+    title: product['title'],
+    category: product['type'],
+    description: product['description'],
+    price: product['price'],
+    rating: product['rating']
+  )
+end
