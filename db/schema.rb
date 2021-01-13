9326490a8e2150713fbf665552ff3b7c192de261
Miguel Pimenta <jmiguelpimenta@gmail.com>
2021-01-13 19:07:25 -0300

    Implementing auth token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_163734) do
+ActiveRecord::Schema.define(version: 2021_01_13_214341) do
 
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_01_13_163734) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "authentication_token", limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
