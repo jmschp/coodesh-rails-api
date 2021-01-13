8132fdbbf4fb4b80a90d88c66d5e7037cb8744ff
Miguel Pimenta <jmiguelpimenta@gmail.com>
2021-01-13 13:41:04 -0300

    Generated model Product

---
+++ db/migrate/20210113163734_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :title, null: false
+      t.string :type, null: false
+      t.text :description
+      t.decimal :price, null: false, default: 0
+      t.integer :rating, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
