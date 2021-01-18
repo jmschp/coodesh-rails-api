adaf0127c901bb22063eec7e5e229533199608ed
Miguel Pimenta <jmiguelpimenta@gmail.com>
2021-01-17 23:05:28 -0300

    Moved file upload test to controllers test

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "upload file" do
+    # visit root_path
+    # click_on "Choose File"
+    #  login_as users(:george)
+
+    file_path = "#{Rails.root}/test/fixtures/files/products-test.json"
+    json_file = fixture_file_upload("#{Rails.root}/test/fixtures/files/products-test.json",'application/json')
+    post upload_validation_url,
+      params: { file: json_file },
+      headers: {
+      content_type: "application/json"
+    }
+    assert_response 201
+  end
 end
--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -11,19 +11,4 @@ class ProductsTest < ApplicationSystemTestCase
     visit root_path
     assert_selector "h2", text: "#{Product.count} products"
   end
-
-  # test "upload file" do
-  #   # visit root_path
-  #   # click_on "Choose File"
-  #   #  login_as users(:george)
-
-  #   file_path = "#{Rails.root}/test/fixtures/files/products-test.json"
-  #   json_file = fixture_file_upload("#{Rails.root}/test/fixtures/files/products-test.json",'application/json')
-  #   post upload_validation_url,
-  #     params: { file: json_file },
-  #     headers: {
-  #     content_type: "application/json"
-  #   }
-  #   assert_response 201
-  # end
 end
